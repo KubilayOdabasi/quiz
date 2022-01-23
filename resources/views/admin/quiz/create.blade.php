301f620c07414d9b0c0bd6f6a2f121e60a4271f4
Kubilay Odabasi <kubilay@kubilayodabasi.com>
2022-01-23 20:54:29 +0300

    Quiz add & Request

--- resources/views/admin/quiz/create.blade.php
+++ resources/views/admin/quiz/create.blade.php
@@ -1,25 +1,24 @@
 <x-app-layout>
     <x-slot name="header">Quiz Oluştur</x-slot>
-
     <div class="card">
         <div class="card-body">
             <form method="post" action="{{ route('quizzes.store') }}">
                 @csrf
                 <div class="form-group">
                     <label>Başlık</label>
-                    <input type="text" name="title" class="form-control" required>
+                    <input type="text" name="title" class="form-control" value="{{ old('title') }}">
                 </div>
                 <div class="form-group">
                     <label>Açıklama</label>
-                    <textarea name="description" class="form-control" rows="4"></textarea>
+                    <textarea name="description" class="form-control" rows="4">{{ old('description') }}</textarea>
                 </div>
                 <div class="form-group">
-                    <input type="checkbox" id="isFinished">
+                    <input type="checkbox" id="isFinished" @if(old('finished_at')) checked @endif>
                     <label>Biriş Tarihi Olacak Mı?</label>
                 </div>
-                <div class="form-group" style="display: none;" id="finished_at_div">
+                <div class="form-group" @if(!old('finished_at')) style="display: none;" @endif id="finished_at_div">
                     <label>Biriş Tarihi</label>
-                    <input type="datetime-local" name="finished_at" class="form-control">
+                    <input type="datetime-local" name="finished_at" class="form-control" value="{{ old('finished_at') }}">
                 </div>
                 <div class="form-group">
                     <button type="submit" class="btn btn-success btn-sm btn-block">Quiz Oluştur</button>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -39,6 +39,19 @@
             <main>
                 <div class="py-12">
                     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                        @if($errors->any())
+                            <div class="alert alert-danger">
+                                @foreach($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </div>
+                        @endif
+                        @if(session('success'))
+                            <div class="alert alert-success">
+                                <i class="fa fa-check"></i>{{ session('success') }}
+                            </div>
+                        @endif
+
                         <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                             {{ $slot }}
                         </div>
